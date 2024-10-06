d369078357ebb95ad4316d26d685e1900c4e73ad
SametGundz <samet-gunduz@hotmail.com>
2024-10-06 22:39:39 +0300

    translating process from turkish to english

--- calibrateCamera.py
+++ calibrateCamera.py
@@ -11,70 +11,70 @@ with open('config.yaml', 'r') as file:
 image_folder = config['paths']['image_folder']
 output_folder = config['paths']['output_folder']
 
-# Logger modülünü kur
+# setting up logger module
 logger = setup_logger()
 
-# Checkerboard boyutları (satır sayısı ve sütun sayısı)
+# checkerboard size (row and collumn size)
 checkerboard_rows = config['checkerboard']['rows']
 checkerboard_columns = config['checkerboard']['columns']
 checkerboard_size = (checkerboard_rows, checkerboard_columns)
 
-# termination criteria (sonlandırma kriteri)
+# termination criteria (max iteration and min reprojection error)
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-# 3D dünya noktalarını hazırlama
+# preparing 3d object points
 objp = np.zeros((checkerboard_size[0] * checkerboard_size[1], 3), np.float32)
 objp[:, :2] = np.mgrid[0:checkerboard_size[0], 0:checkerboard_size[1]].T.reshape(-1, 2)
 
-# 3D ve 2D noktaları depolamak için listeler
+# lists for store up 3d and 2d points
 objpoints = []  # 3D noktalar 
 imgpoints = []  # 2D noktalar 
 
 reprojection_errors = []
 
-# Checkerboard görüntülerini yükleme 
+# uploading checkerboard images
 images = glob.glob(os.path.join(image_folder, '*.jpg'))
 counter = 0 
 
-logger.info(f'{len(images)} adet resim bulundu. İşleme başlıyor...')
+logger.info(f'{len(images)} images found. Starting processing...')
 
 for image in images:
     img = cv.imread(image)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    # Checkerboard köşelerini bulma
+    # finding checkerboard corners
     ret, corners = cv.findChessboardCorners(gray, checkerboard_size, None)
 
     if ret == True:
-        logger.info(f"Köşeler bulundu: {image}")
-        # Köşeleri sub-pixel doğruluğunda iyileştir
+        logger.info(f"Corners found: {image}")
+        # optimizing corners using sub-pixel accuracy
         corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
 
-        # 3D dünya noktaları ve görüntüdeki köşe noktalarını depola
+        # store up 3d object points and corners of checkerboard
         objpoints.append(objp)
         imgpoints.append(corners2)
 
-        # Bulunan köşeleri çiz ve göster
+        # draw the detected corners and display the image
         cv.drawChessboardCorners(img, checkerboard_size, corners2, ret)
 
         output_path = os.path.join(output_folder, f'img{counter}.jpg')
         cv.imwrite(output_path, img)
     else:
-        logger.warning(f"Köşeler bulunamadı: {image}")
+        logger.warning(f"Corners not found: {image}")
     counter += 1
 
-# Eğer hiç checkerboard bulunmadıysa, hata mesajı ver
+# show this logger message if there is no any checkerboard
 if not objpoints or not imgpoints:
-    logger.error("Hiçbir checkerboard köşesi bulunamadı. Program sonlandırılıyor.")
+    logger.error("No checkerboard corners found. Terminating the program.")
     exit()
 
-# Kamerayı kalibre etme
-logger.info("Kamera kalibrasyonu başlatılıyor...")
+# calibrating camera
+logger.info("Starting camera calibration...")
 ret, intrinsic_matrix, distortion_coeff, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-logger.info("Kamera kalibrasyonu tamamlandı")
+logger.info("Camera calibration completed.")
 
-# Reprojection Error Hesaplama
+# calculating reprojection error
 mean_error = 0
 for i in range(len(objpoints)):
     imgpoints2, _ = cv.projectPoints(objpoints[i], rvecs[i], tvecs[i], intrinsic_matrix, distortion_coeff)
@@ -83,11 +83,11 @@ for i in range(len(objpoints)):
     logger.info(f'Reprojection Error for image {i+1}: {error}')
 
 mean_error = np.mean(reprojection_errors)
-logger.info(f"Ortalama Reprojection Error: {mean_error}")
+logger.info(f"Average Reprojection Error: {mean_error}")
 logger.info(f"Intrinsic Matrix:\n {intrinsic_matrix}")
 logger.info(f"Distortion Coefficients:\n {distortion_coeff}")
 
-# Extrinsic ve intrinsic parametreleri kaydetme
+# saving the extrinsic and intrinsic values
 extrinsic_data = {
     'rvecs': [rvec.tolist() for rvec in rvecs],
     'tvecs': [tvec.tolist() for tvec in tvecs],
@@ -96,13 +96,13 @@ extrinsic_data = {
     'reprojection_errors': reprojection_errors
 }
 
-# Zaman damgası ile dosya adı oluşturuluyor
+# creating file with a time stamps
 timestamp = time.strftime("%Y%m%d_%H%M%S")
 output_filename = f"calibration_{timestamp}.yaml"
 output_path = os.path.join(output_folder, output_filename)
 
-# YAML dosyasına yazma işlemi
+# writing on YAML file
 with open(output_path, 'w') as file:
     yaml.dump(extrinsic_data, file)
 
-logger.info(f"Extrinsic ve intrinsic değerler {output_path} dosyasına kaydedildi.")
+logger.info(f"Extrinsic and intrinsic values saved to the file {output_path}.")
